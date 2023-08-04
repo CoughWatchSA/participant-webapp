ab07a1f16f483b51d0a90984c3fefadd1283f3c4
Matteo Delfino <matteo.delfino@isi.it>
2023-08-04 17:53:10 +0200

    remove unused faHome icon

--- src/index.tsx
+++ src/index.tsx
@@ -13,7 +13,6 @@ import '@fontsource/open-sans';
 import '@fontsource/open-sans/400-italic.css';
 import '@fontsource/open-sans/700.css';
 import {
-  faHome,
   faSignOutAlt,
   faCog,
   faArrowRight,
@@ -35,7 +34,7 @@ const fallbackLanguage = process.env.REACT_APP_FALLBACK_LANGUAGE ?? 'en';
 
 initI18n(defaultLanguage, fallbackLanguage, localeURL);
 
-library.add(faHome, faSignOutAlt, faCog, faArrowRight, faArrowLeft, faBars, faPen, faTrash, faTwitter, faFacebook);
+library.add(faSignOutAlt, faCog, faArrowRight, faArrowLeft, faBars, faPen, faTrash, faTwitter, faFacebook);
 dom.watch();
 
 ReactDOM.render(
